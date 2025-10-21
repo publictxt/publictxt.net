cf5a47f14f5471784c5870e86e473661381d3f86
Jaysen Naidoo <jaysen@gmail.com>
2025-10-21 17:06:51 +0200

    misc defaults

--- src/Core/PublicTxt.Core/Models/TxtInstance.cs
+++ src/Core/PublicTxt.Core/Models/TxtInstance.cs
@@ -3,19 +3,19 @@ namespace PublicTxt.Core.Models;
 public class TxtInstance
 {
     // Identity
-    public Guid Id { get; set; }
+    public Guid Id { get; set; } = Guid.NewGuid();
     public required string Name { get; set; }
     public string? LocalPath { get; set; }
     
     // Git Info
     public string? RemoteUrl { get; set; }
     public string? CurrentBranch { get; set; } = "main";
-    public GitStatus GitStatus { get; set; }
+    public GitStatus GitStatus { get; set; } = GitStatus.Unknown;
     public DateTimeOffset LastGitSync { get; set; }
     
     // Metadata
-    public InstanceStatus Status { get; set; }
-    public DateTimeOffset CreatedAt { get; set; }
+    public InstanceStatus Status { get; set; } = InstanceStatus.Uninitialized;
+    public DateTimeOffset CreatedAt { get; set; } = DateTimeOffset.UtcNow;
     public DateTimeOffset? LastAccessedAt { get; set; }
 
     // Configuration
